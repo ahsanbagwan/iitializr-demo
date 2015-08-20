f82749e8ceffabf9ecee61530a3c1c61df12f1a5
Ahsan Bagwan <ahsanbagwan@gmail.com>
2015-08-20 15:01:40 +0530

    Brought server.js in root

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-console.log("dirname " + __dirname);
 app.use(express.static(__dirname));
 
 app.listen(process.env.PORT || 3000);
-console.log("Server started");
